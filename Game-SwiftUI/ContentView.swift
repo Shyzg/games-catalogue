2d40a2044a360395f81b0869c96182d060b5c041
Al Jarreau Arman Maulana <sn@shyzagonakamoto.dev>
2022-05-19 08:35:12 +0700

    🟢 Push-ing App.

---
+++ Game-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Game-SwiftUI
+//
+//  Created by Shyzago Nakamoto on 19/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Game-SwiftUI/Game_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_SwiftUIApp.swift
+//  Game-SwiftUI
+//
+//  Created by Shyzago Nakamoto on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Game_SwiftUIApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
